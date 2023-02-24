fa7bf6de0c22b44a9934a022130bebfe0d10d1a9
Marc A. Donis <marc@balloonary.io>
2023-02-24 20:26:56 +0100

    .

--- main.js
+++ main.js
@@ -3,6 +3,26 @@ function load() {
 		el.addEventListener('click', wordClick);
 	}
 	setWords();
+	document.getElementById('player-name').value =
+		localStorage.valuesPlayerName || '';
+	if (localStorage.valuesPlayerName) {
+		refreshResults();
+	}
+
+	let options = '';
+	for (const usernameKey of Object.keys(localStorage).filter(
+		(key) =>
+			key.endsWith('-values-results') &&
+			(!localStorage.valuesPlayerName ||
+				key != `${localStorage.valuesPlayerName}-values-results`)
+	)) {
+		const username = usernameKey.substring(
+			0,
+			usernameKey.length - '-values-results'.length
+		);
+		options += `<option>${username}</option>`;
+	}
+	document.getElementById('other-player-select').innerHTML = options;
 }
 
 function setWords() {
@@ -15,7 +35,8 @@ function setWords() {
 function wordClick(el) {
 	const playerName = document.getElementById('player-name').value;
 	if (playerName) {
-		const results = JSON.parse(localStorage[`${playerName}-results`] || '{}');
+		localStorage.valuesPlayerName = playerName;
+		const results = getResults(playerName);
 		const winnerWord = el.target.innerHTML;
 		results[winnerWord] = {
 			rounds: (results[winnerWord]?.rounds || 0) + 1,
@@ -28,12 +49,49 @@ function wordClick(el) {
 		).innerHTML;
 		results[loserWord] = {
 			rounds: (results[loserWord]?.rounds || 0) + 1,
-			wins: results[winnerWord]?.wins || 0,
+			wins: results[loserWord]?.wins || 0,
 		};
-		console.log(results);
-		localStorage[`${playerName}-results`] = JSON.stringify(results);
+		// console.log(results);
+		localStorage[`${playerName}-values-results`] = JSON.stringify(results);
+
+		refreshResults();
 		setWords();
 	} else {
 		document.getElementById('player-name').focus();
 	}
+}
+
+function refreshResults(playerName) {
+	playerName = playerName || localStorage.valuesPlayerName;
+	const results = getResults(playerName);
+	let html = '<table>';
+	html += '<tr><th>Word</th><th>Score</th></tr>';
+	const sortedWords = Object.keys(results).sort((a, b) => {
+		// console.log(a, b);
+		// console.log(a.wins, b.wins);
+		// console.log(a.rounds, b.rounds);
+		return results[a].wins / results[a].rounds >
+			results[b].wins / results[b].rounds
+			? -1
+			: 1;
+	});
+	for (const word of sortedWords) {
+		const score = Math.round((100 * results[word].wins) / results[word].rounds);
+		html += `<tr><td>${word}</td><td>${score}</td></tr>`;
+	}
+	html += '</table>';
+	document.getElementById(
+		playerName == localStorage.valuesPlayerName
+			? 'user-results'
+			: 'other-user-results'
+	).innerHTML = html;
+}
+
+function getResults(playerName) {
+	return JSON.parse(localStorage[`${playerName}-values-results`] || '{}');
+}
+
+function setOtherUserResults() {
+	const username = document.getElementById('other-player-select').value;
+	refreshResults(username);
 }
